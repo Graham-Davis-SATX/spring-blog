2f0a1af5000ef2d6ebd44844394cf5bc49077f55
Graham Davis <davis.graham.satx@gmail.com>
2018-09-05 16:22:04 -0500

    pushing controllers and views lectures

---
+++ src/main/java/com/blog/blog/MathController.java
@@ -0,0 +1,32 @@
+package com.blog.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MathController {
+
+    @RequestMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public String addNums(@PathVariable int num1, @PathVariable int num2) {
+        return num1 + " plus " + num2 + " equals " + (num1 + num2);
+    }
+
+    @RequestMapping("/subtract/{num1}/from/{num2}")
+    @ResponseBody
+    public String subNums(@PathVariable int num1, @PathVariable int num2) {
+        return num2 + " minus " + num1 + " equals " + (num2 - num1);
+    }
+
+    @RequestMapping("/multiply/{num1}/by/{num2}")
+    @ResponseBody
+    public String multNums(@PathVariable int num1, @PathVariable int num2) {
+        return num1 + " times " + num2 + " equals " + (num1 * num2);
+    }
+
+    @RequestMapping("/divide/{num1}/by/{num2}")
+    @ResponseBody
+    public String divNums(@PathVariable int num1, @PathVariable int num2) {
+        return num1 + " divided " + num2 + " equals " + (num1 / num2);
+    }
+}
---
+++ src/main/java/com/blog/blog/PostController.java
@@ -0,0 +1,38 @@
+package com.blog.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostController {
+
+//    GET	/posts	posts index page
+//    GET	/posts/{id}	view an individual post
+//    GET	/posts/create	view the form for creating a post
+//    POST	/posts/create	create a new post
+
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String index() {
+        return "Showing all posts";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String show(@PathVariable long id) {
+        return "Showing post with id of " + id;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String show() {
+        return "Showing form to create new post!";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String create() {
+        return "New post was created!";
+    }
+}
